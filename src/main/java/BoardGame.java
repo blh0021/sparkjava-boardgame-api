95daa7fbecace264c1f726178df17ad6ebe99aa0
Benjamin Harrington <ben.harrington@manheim.com>
2017-01-19 08:51:42 -0600

    initial rest api hello

---
+++ src/main/java/BoardGame.java
@@ -0,0 +1,14 @@
+import static spark.Spark.*;
+
+public class BoardGame {
+    public static void main(String[] args) {
+        String gamePort = System.getenv("GAME_PORT");
+        port((gamePort != null ) ? Integer.parseInt(gamePort) : 8080);
+
+        String gameIP = System.getenv("GAME_IP");
+        ipAddress((gameIP != null) ? gameIP : "0.0.0.0");
+
+        get("/", (req, res) -> "Board Game API");
+        get("/hello", (req, res) -> "{ \"greeting\": \"Hello World\" }");
+    }
+}
