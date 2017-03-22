b556b22893125cbaf171b0e7853079072a8b3940
Ben Harrington <ben@harringtonweb.com>
2017-03-22 07:51:33 -0500

    return next move

--- src/main/java/BoardGame.java
+++ src/main/java/BoardGame.java
@@ -1,5 +1,10 @@
 import static spark.Spark.*;
 import static com.github.blh0021.TicTacToe.*;
+import java.io.IOException;
+
+import com.fasterxml.jackson.core.JsonGenerationException;
+import com.fasterxml.jackson.databind.JsonMappingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
 public class BoardGame {
     public static void main(String[] args) {
@@ -11,7 +16,21 @@ public class BoardGame {
 
         get("/", (req, res) -> "Board Game API");
 
-        //TicTacToe ttt = new TicTacToe();
         get("/hello", (req, res) -> hello());
+
+        post("/next-move", (req, res) -> {
+            ObjectMapper mapper = new ObjectMapper();
+            try {
+              NextMove nm = mapper.readValue(req.body(), NextMove.class);
+              return nextMove(nm.getBoard(), nm.getPiece());
+            } catch (JsonGenerationException e) {
+                e.printStackTrace();
+            } catch (JsonMappingException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return "";
+        });
     }
 }
---
+++ src/main/java/NextMove.java
@@ -0,0 +1,21 @@
+public class NextMove {
+
+  	private String board;
+  	private char piece;
+
+    public String getBoard() {
+        return this.board;
+    }
+
+    public void setBoard(String board) {
+        this.board = board;
+    }
+
+    public char getPiece() {
+        return this.piece;
+    }
+
+    public void setPiece(char piece) {
+        this.piece = piece;
+    }
+}
